5ca735c49ce0e6983ff2cd06860644efb4d34d1e
Lorenzo Sciandra <lsciandra@microsoft.com>
2023-02-08 14:39:57 +0000

    still trying things around

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'react-native',
   rootDir: './__tests__',
   testTimeout: 60000,
   bail: 0,
 };
+
+module.exports = config;
